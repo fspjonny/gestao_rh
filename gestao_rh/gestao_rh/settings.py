33a0429b9617512a6f0a1aa5b9a62161835a8fdd
fspjonny <fabiosilvapedro@yahoo.com.br>
2019-12-03 19:42:50 -0300

    Commit de segurança do Settings.py

--- gestao_rh/gestao_rh/settings.py
+++ gestao_rh/gestao_rh/settings.py
@@ -1,4 +1,5 @@
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,10 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^a)&8_9sbu@v^g4f87!cwobt6$!kwcl_sn9jx$^om-%l_-qanj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
