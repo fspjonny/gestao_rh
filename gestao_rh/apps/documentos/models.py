37f22dc7a6143a7a535b45b666a7973af4e463d9
fspjonny <fabiosilvapedro@yahoo.com.br>
2019-12-05 12:21:45 -0300

    Commit antes de rodar migrations para relacionamento dos models

--- gestao_rh/apps/documentos/models.py
+++ gestao_rh/apps/documentos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.colaboradores.model import Colaborador
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100, verbose_name='Documento', help_text='documento')
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.descricao
--- gestao_rh/apps/registro_hora_extra/models.py
+++ gestao_rh/apps/registro_hora_extra/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from apps.colaboradores.models import Colaborador
+
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=150, verbose_name='Motivo', help_text='motivo da hora extra')
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.motivo
