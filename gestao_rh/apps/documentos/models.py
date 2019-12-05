ec806b54b8814fc8f303b5e52763ddf4d84d8527
fspjonny <fabiosilvapedro@yahoo.com.br>
2019-12-05 13:43:16 -0300

    Migrations efetuadas e relacionamentos de banco de dados

--- gestao_rh/apps/documentos/models.py
+++ gestao_rh/apps/documentos/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from apps.colaboradores.model import Colaborador
+from apps.colaboradores.models import Colaborador
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100, verbose_name='Documento', help_text='documento')
-    colaborador = models.ForeignKey(Colaborador, on_delete=models.SET_NULL, null=True, blank=True)
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
--- gestao_rh/apps/registro_hora_extra/admin.py
+++ gestao_rh/apps/registro_hora_extra/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import RegistroHoraExtra
 
+class FizeramHorasExtras(admin.ModelAdmin):
+    list_display = ('colaborador', 'motivo')
+    list_filter = ['colaborador',]
 
-admin.site.register(RegistroHoraExtra)
+admin.site.register(RegistroHoraExtra, FizeramHorasExtras)
 
-#TODO: Aguardando resposta do forum para remover Underlines do nome REGISTRO_HORA_EXTRA.
-# Não foi feito PUSH para o Github dos Apps(documentos e registro_hora_extra)
